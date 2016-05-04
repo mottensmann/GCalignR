8088be1d5529aeafd8e3134e50786151434faefa
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-05-04 09:30:04 +0200

    first package version

--- R/align_chromatograms.R
+++ R/align_chromatograms.R
@@ -1,143 +1,154 @@
-
+#' Aligning chromatograms based on retention times
+#'
+#' @param datafile
+#' @param rt_name
+#'
+#' @return
+#'
+#'
+#' @references
+#'
+#' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
+#'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
+#'
+#' @export
+#'
 
 
 align_chromatograms <- function(datafile, rt_name = "RT", rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = NULL,
                                 step1_maxshift = 0.05, step2_maxshift = 0.02, step3_maxdiff = 0.05, blanks = NULL,
                                 del_single_sub = FALSE) {
-    
+
     # extract names
     ind_names <- readr::read_lines(datafile, n_max = 1) %>%
         stringr::str_split(pattern = "\t") %>%
         unlist() %>%
         .[. != ""]
-    
+
     # extract column names
     col_names <- readr::read_lines(datafile, n_max = 1, skip = 1) %>%
         stringr::str_split(pattern = "\t") %>%
         unlist() %>%
         .[. != ""]
-    
+
     # extract data
     chroma <- read.table(datafile, skip = 2, sep = "\t", stringsAsFactors = F)
     # how to deal with commas?
     chroma <- as.data.frame(apply(chroma, 2, function(x) str_replace_all(x, ",", ".")))
     chroma <-  as.data.frame(apply(chroma, 2, as.numeric))
-    
+
     # check 1
     if (!((ncol(chroma) / length(col_names)) %% 1) == 0) stop("number of data columns is not a multiple of the column names provided")
     # check 2
     if (!((ncol(chroma) / length(col_names))  == length(ind_names))) stop("the number of individual ids provided does not fit to the number of columns in the data")
-    
+
     # matrix to list
-    source("R/conv_gc_mat_to_list.R")
-    source("R/rename_cols.R")
+    # source("R/conv_gc_mat_to_list.R")
+    # source("R/rename_cols.R")
     chromatograms <- conv_gc_mat_to_list(chroma, ind_names, var_names = col_names)
-    
-    
+
+
     # Start of processing --------------------------------------------------------------------------
-    
-    source("R/rt_cutoff.R")
+
+    # source("R/rt_cutoff.R")
     # 1.) cut retention times below 8
     chromatograms <- lapply(chromatograms, rt_cutoff, low = rt_cutoff_low, high = rt_cutoff_high, rt_col_name = rt_name)
-    
-    source("R/linear_transformation.R")
+
+    # source("R/linear_transformation.R")
     # 2.) Linear Transformation of Retentiontimes
-    
-    ## thinking about reference: default is chromatogram with most peaks - optional: manual 
+
+    ## thinking about reference: default is chromatogram with most peaks - optional: manual
     chroma_aligned <- linear_transformation(chromatograms, shift=step1_maxshift, step_size=0.01, error=0, reference = reference, rt_col_name = rt_name)
-    
+
     # Make List equal in length
-    source("R/matrix_append.R")
+    # source("R/matrix_append.R")
     chromatograms <- lapply(chroma_aligned, matrix_append, chroma_aligned)
-    
-    
-    source("R/evaluate_chroma.R")
+
+
+    # source("R/evaluate_chroma.R")
     Length <- (max(unlist(lapply(chromatograms, function(x) out <- nrow(x))))) # To obtain Rows after run of the algorithm
     Variation <- mean(var_per_row(chromatograms),na.rm = T)
-    
+
     #rm(list=c("Chroma_aligned","chroma","AdjustRetentionTime","AlignPeaks","BestShift",
     #          "RetentionCutoff","SharedPeaks","PeakShift"))
-    
-    source("R/align_individual_peaks.R")
-    source("R/shift_rows.R")
+
+    # source("R/align_individual_peaks.R")
+    # source("R/shift_rows.R")
     chromatograms_aligned <- align_individual_peaks(chromatograms, error_span = step2_maxshift, n_iter = 1)
-    
-    
+
+
     average_rts <- mean_per_row(chromatograms_aligned)
-    
+
     # delete empty rows (if existing)
     chromatograms <- lapply(chromatograms_aligned, function(x) {
         keep_rows <- which(!is.na(average_rts))
         out <- x[keep_rows, ]
     })
-    
+
     # still empty rows?
     rt_mat1 <- do.call(cbind, lapply(chromatograms, function(x) x$RT))
     rowSums(rt_mat1>0)
-    
-    source("R/merge_rows.R")
+
+    # source("R/merge_rows.R")
     average_rts <- mean_per_row(chromatograms)
-    
+
     # min distance here is crucial --> depends on sample size
-    chroma_merged <- merge_redundant_rows(chromatograms, average_rts, min_distance=step3_maxdiff = 0.05)
-    
+    chroma_merged <- merge_redundant_rows(chromatograms, average_rts, min_distance=step3_maxdiff)
+
     average_rts <- mean_per_row(chroma_merged)
     rt_mat2 <- do.call(cbind, lapply(chroma_merged, function(x) x$RT))
-    
-    
-    
-    
-    
+
+
     average_rts <- mean_per_row(chroma_merged)
-    
+
     # delete empty rows
     del_empty_rows <- function(chromatogram, average_rts){
         chromatogram <- chromatogram[!is.na(average_rts), ]
         chromatogram
     }
     chromatograms <- lapply(chromatograms, del_empty_rows, average_rts)
-    
-    
+
+
     # delete blanks
     if (!is.null(blanks)) {
         # delete one blank
         delete_blank <- function(blank, chromatograms) {
             del_substances <- which(chromatograms[[blank]]$RT > 0)
-            chroma_out <- lapply(chromatograms, function(x) x[-del_substances, ])    
+            chroma_out <- lapply(chromatograms, function(x) x[-del_substances, ])
         }
         # delete all blanks
         for (i in blanks) {
             chromatograms <- delete_blank(i, chromatograms)
         }
     }
-   
-    
+
+
     # delete single substances
     # create matrix with all retention times
     rt_mat <- do.call(cbind, lapply(chromatograms, function(x) x$RT))
-    
+
     if (del_single_sub) {
         # find single retention times in rows
         single_subs_ind <- which(rowSums(rt_mat > 0) == 1)
         # delete substances occuring in just one individual
-        chromatograms <- lapply(chromatograms, function(x) x[-single_subs_ind, ]) 
+        chromatograms <- lapply(chromatograms, function(x) x[-single_subs_ind, ])
     }
 
     # calculate final retention times
     rt_mat <- do.call(cbind, lapply(chromatograms, function(x) x$RT))
-    
+
     # mean per row without 0
     row_mean <- function(x) {
         if (all(x==0)) 0 else mean(x[x!=0])
     }
     mean_per_row <- apply(rt_mat,1, row_mean)
-    
+
     # create abundance matrix
     area_df <- as.data.frame(do.call(cbind, lapply(chromatograms, function(x) x$Area)))
-    
+
     # rownames
     row.names(area_df) <- mean_per_row
-    
+
 }
 
 
--- R/merge_rows.R
+++ R/merge_rows.R
@@ -1,123 +1,112 @@
 #' merges redundant rows
-#' 
+#'
 #' @param chromatograms \code{data.frame} containing GC data (retention times, peak area, peak hight etc) for
 #'   one individual in adjacent columns. The first column for all individuals has to be the retention
 #'   time, retention time has to be named RT.
 #' @param average_rts average retention times across samples per row
 #' @param min_distance difference between the mean retention time of two rows of the chromatograms
-#'        to be considered for merging if no individual has substances in both rows. 
-#' 
-#' @return 
+#'        to be considered for merging if no individual has substances in both rows.
+#'
+#' @return
 #' chromatograms with merged rows
 #'
 #' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
 #'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'  
-#'  
+#'
+#'
 merge_redundant_rows <- function(chromatograms, average_rts,  min_distance=0.05){
-    
+
     Merging <- 'Start'
     while(Merging != 'Stop'){
-        
-        # Find and delete Rows that are 
+
+        # Find and delete Rows that are
         # (i) similar in their Retention Time
         # (ii) redundant i.e. no chromatogram contains substances in both rows
         # Always start to pick the fist potential candidate of row-pairs to merge
         # in case merging is not aplicable, take the next position
         # after merging of one pair, update criteria
         # if non is ready to merge anymore, stop the merging algorithm
-        
+
         # Updating merging criterions
         average_rts <- mean_per_row(chromatograms) # Average RTs, after merging rows
         similar <- similar_rows(average_rts, min_distance)    # remaining similarities
         # print(similar)
-       
+
        #  if (sum(similar > nrow(chromatograms[[1]])) > 0) similar <- similar[!(similar > nrow(chromatograms[[1]]))]
         counter <- 1
         while (counter!='Stop'){
             # loop through vector of similar rows, until one shift was done
             # print(paste0("counter=", counter))
-           
+
             # check if any chromatogram contains substances in both rows
             if (length(similar) == 0){
                 Merging <- "Stop"
                 break
-            } 
+            }
             redundant <- sapply(lapply(chromatograms, check_redundancy, similar[counter]),as.vector) #Check first position
-            
+
             # checks whether all individuals just have substances in one of the rows ("Strict")
             # checks whether at least 95% of individuals just have substances in one of the rows ("Proportional")
             criterion <- is_redundant(redundant = redundant, criterion="strict")
-            
+
             if (criterion == 1){ # only merge if criterion proves redundancy of one of the rows
                 chromatograms <- lapply(chromatograms, merge_rows, to_merge = similar[counter], criterion="zero")
-                
+
                 # check2 <- similar[counter]
                 counter <- 'Stop'
-                
+
                # check <- do.call(cbind, lapply(chromatograms, function(x) x$RT))
-                
+
             } else if  (criterion == 0) {
                 counter <- counter+1
-             
+
                 if (counter>length(similar)){
                     Merging <- 'Stop'
                     counter <- 'Stop'
-                }  
-               
-                # print(criterion)
-                # print(redundant)
-                # print(Merging)
-                
+                }
             }
-            # print(paste0("counter = ", counter))
-            # print(paste0("similar = ", length(similar)))
-            # print(paste0("whichsimilar = ", check2))
-            # print(paste0("criterion = ", criterion ))
-            # print(paste0("nrow = ", nrow(chromatograms[[1]]) ))
-            # print(check[c(nrow(check), nrow(check)-1), ])
         }
     }
-    
+
     chromatograms
 }
 
 
 #' Estimate degree of similarity between subsequent rows by comparison of mean retention times of rows.
-#' 
+#'
 #' @param average_rts average retention times per row across individuals
-#' @param  min_distance default 0.05, 
+#' @param  min_distance default 0.05,
 #' @details Similarity is evaluated at the level of min_distance, given in seconds
-#' @return 
+#' @return
 #' position of a row, that is similar to the previous, i.e. row 5 is similar to 4
 #'
 #' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
 #'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'  
-#'  
-#'  
+#'
+#'
+#'
 similar_rows <- function(average_rts, min_distance=0.05){
-    
+
     difference <- rep(NA, (length(average_rts)-1)) # Estimate Difference between adjacent rows
-    
+
     for (i in 2:length(average_rts)){
-        difference[i] <- average_rts[i]-average_rts[i-1]  
+        difference[i] <- average_rts[i]-average_rts[i-1]
     }
     similar <- which(difference <= min_distance) # Which rows differ less the MinDistance ?
     similar
 }
 
 #' If only one of two neighbouring rows contain a substancethey are redundant, coded by a One
-#' 
+#'
 #' @param chromatograms
 #' @param  similar
-#' 
-#' @return 
+#'
+#' @return
 #' if similar rows are redundant, 1, else 0
 #'
 #' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
 #'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'  
+#'
 
 check_redundancy <- function(chromatogram, similar){
     # If only one of two neighbouring rows contain a substance
@@ -132,20 +121,20 @@ check_redundancy <- function(chromatogram, similar){
 }
 
 #' Indicates by a binary output variable (1/0) if rows should be merged
-#' 
+#'
 #' @param chromatograms
 #' @param  similar
-#' 
+#'
 #' @details  Methods: "strict": A single sample with two peaks prevents merging
 #           "proportional": Merging is acceptabel if only 5 % of samples show two peaks
-#' @return 
+#' @return
 #' if similar rows are redundant, 1, else 0
 #'
 #' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
 #'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'  
-#'  
-#'  
+#'
+#'
+#'
 is_redundant <- function(similar, redundant, criterion="strict"){
     # Indicates by a binary output variable (1/0) if rows should be merged
     # Methods: Strict: A single sample with two peaks prevents merging
@@ -163,22 +152,22 @@ is_redundant <- function(similar, redundant, criterion="strict"){
     ToMerge
 }
 
-#' algorithm to adjust unreliability of chromatograms on an individual peak basis 
-#' 
+#' algorithm to adjust unreliability of chromatograms on an individual peak basis
+#'
 #' @param chromatogram \code{data.frame} containing GC data (retention times, peak area, peak hight etc) for
 #'   one individual in adjacent columns. The first column for all individuals has to be the retention
 #'   time, retention time has to be named RT.
 #' @param to_merge Last row of a similar pair
-#' @param criterion "Zero" - delete a row containing zeros, or 
+#' @param criterion "Zero" - delete a row containing zeros, or
 #'        "area": take the larger of two peaks, defined by the are of the peaks. "area" is just
 #'        useful when redundancy criterion was "proportional" instead of the default "strict".
-#' @return 
+#' @return
 #' aligned chromatograms
 #'
 #' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
 #'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'  
-#'  
+#'
+#'
 merge_rows <- function(chromatogram, to_merge, criterion="zero"){
     # Check always the row containing just zeros, in case of zeros in both, just delete one of them
     # To Merge == Last row of a similar pair
@@ -188,7 +177,7 @@ merge_rows <- function(chromatogram, to_merge, criterion="zero"){
     R2 <- chromatogram$RT[Row2]
     if (criterion=="zero"){
        if (Row1 > 1 & Row2 < nrow(chromatogram)){ # Avoid taking first and last rows
-            if (R1 == 0){ 
+            if (R1 == 0){
                 #  Delete Row1
                 chromatogram <- rbind(chromatogram[1:(Row1-1), ], chromatogram[Row2:nrow(chromatogram), ])
             } else if (R2 == 0){
@@ -196,16 +185,16 @@ merge_rows <- function(chromatogram, to_merge, criterion="zero"){
                 chromatogram <- rbind(chromatogram[1:Row1,],chromatogram[(Row2+1):nrow(chromatogram), ])
             }
        }
-       
+
       if (Row2 == nrow(chromatogram)){
-          if (R1 == 0) { 
+          if (R1 == 0) {
             chromatogram <- rbind(chromatogram[1:(Row1-1), ], chromatogram[Row2:nrow(chromatogram), ])
         } else if (R2 == 0){
             chromatogram <- chromatogram[1:Row1, ]
         }
       }
     }
-        
+
     if(criterion=="area"){ # Take the larger of two peaks, defined by the are of the peaks
         if (chromatogram$Area[Row1] >= chromatogram$Area[Row2]){
             chromatogram <- rbind(chromatogram[1:Row1,] ,chromatograms[(Row2+1):nrow(chromatogram), ])

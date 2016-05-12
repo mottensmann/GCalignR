fd5954f95b05d623597b9fad7f97c0aae1dd0208
mottensmann <Meinolf.Ottensmann@web.de>
2016-05-12 11:07:45 +0200

    function for rt extractions

---
+++ R/rt_extract_heatmap.R
@@ -0,0 +1,50 @@
+#' rt_extract_heatmap
+#'
+#' @param chromatograms List of chromatograms
+#'
+#' @param blanks character vector of blanks
+#'
+#' @param del_single_sub logical, indicating wheter single substances are deleted
+#'
+#' @param rt_name chracter, specifies column holding retention times
+#'
+#' @return
+#'
+#'
+#' @references
+#'
+#' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
+#'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
+#'
+#' @export
+#'
+rt_extract_heatmap <- function(chromatograms,blanks,rt_name,del_single_sub){
+
+
+    # delete blanks
+    if (!is.null(blanks)) {
+        # delete one blank
+        delete_blank <- function(blank, chromatograms) {
+            del_substances <- which(chromatograms[[blank]]$RT > 0)
+            chroma_out <- lapply(chromatograms, function(x) x[-del_substances, ])
+        }
+        # delete all blanks
+        for (i in blanks) {
+            chromatograms <- delete_blank(i, chromatograms)
+        }
+    }
+
+    # delete single substances
+    # create matrix with all retention times
+    rt_mat <- do.call(cbind, lapply(chromatograms, function(x) x[[rt_name]]))
+
+    if (del_single_sub) {
+        # find single retention times in rows
+        single_subs_ind <- which(rowSums(rt_mat > 0) == 1)
+        # delete substances occuring in just one individual
+        chromatograms <- lapply(chromatograms, function(x) x[-single_subs_ind, ])
+    }
+
+    # calculate final retention times
+    rt_mat <- do.call(cbind, lapply(chromatograms, function(x) x[[rt_name]]))
+}

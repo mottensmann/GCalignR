06f192749293c6a161fea0c0be6155bc5fa540e9
mottensmann <Meinolf.Ottensmann@web.de>
2016-05-20 12:04:05 +0200

    aling_var calculates range of coefficient of variance for peaks

---
+++ R/align_var.R
@@ -0,0 +1,39 @@
+#' Quantify goodness of alignment by relative variances of peak retention times
+#'
+#' @description
+#' \code{align_var()} calculates range and mean of the coefficient of variation for retention
+#' times of peaks that are currently within the same row in data.frames of \code{gc_peak_list}
+#' (i.e. classified as same substance).
+#'
+#' @inheritParams matrix_append
+#'
+#' @inheritParams align_chromatograms
+#'
+#' @return
+#' a list of two numeric vectors
+#' \item{range}{the minimum and maximum relative variation of peak retention times}
+#' \item{average}{the mean relative variation of peak retention times}
+#'
+#'
+#' @export
+#'
+
+align_var <- function(gc_peak_list,rt_col_name){
+
+variance_in_peaks <- function(gc_peak_list, sample_indices, peak, rt_col_name){
+    rt <- unlist(lapply(gc_peak_list[sample_indices], function(x) x[peak, rt_col_name]))
+    var_rt <- sd(rt[!(rt == 0)], na.rm = TRUE)
+    rel_var_rt <- var_rt/mean(rt)
+    rel_var_rt
+}
+    peak <- nrow(gc_peak_list[[1]])
+    out <- unlist(lapply(1:peak,
+    function(x) variance_in_peaks(gc_peak_list, 1:length(gc_peak_list), x, rt_col_name)))
+
+    output <- list(range=range(out,na.rm = T),average=mean(out,na.rm=T))
+
+    return(output)
+}
+
+
+

d6835204eab22ae2eccfb4e07f5e0499cfc1a490
mottensmann <Meinolf.Ottensmann@web.de>
2016-05-20 09:13:22 +0200

    functions outsourced

--- R/evaluate_chroma.R
+++ R/evaluate_chroma.R
@@ -1,31 +1,3 @@
-#' Calculates mean retention times
-#'
-#' @description Calculates mean of retention times up to a certain sample (Zeros are ignored)
-#'
-#' @param chromatograms list of data.frames containing GC data (retention times, peak area, peak hight etc) for
-#'   individual samples in adjacent columns.
-#'
-#' @param samples indices of samples up to sample of interest (1:sample-1).
-#'
-#' @param retention_row current retention time row to be compared.
-#'
-#' @param rt_col_name name of retention time column.
-#'
-#' @return
-#' mean retention times
-#'
-#' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
-#'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'
-#' @export
-#'
-
-mean_of_samples <- function(chromatograms, samples, retention_row, rt_col_name){
-    rts <- unlist(lapply(chromatograms[samples], function(x) x[retention_row, rt_col_name]))
-    mean_rt <- mean(rts[!(rts == 0)], na.rm = TRUE)
-
-}
-
 #' calculates mean of retention times up to a certain sample (not including 0s)
 #'
 #' @param chromatograms \code{data.frame} containing GC data (retention times, peak area, peak hight etc) for
@@ -52,28 +24,7 @@ var_of_samples <- function(chromatograms, samples, retention_row, rt_col_name){
 }
 
 
-#' calculates mean retention time per row
-#'
-#' @param chromatograms data.frame containing GC data (retention times, peak area, peak hight etc) for
-#'   one individual in adjacent columns. The first column for all individuals has to be the retention
-#'   time, retention time has to be named RT.
-#'
-#'   @param rt_col_name character string denoting the column containing retention times in \code{chromatograms}
-#' @return
-#' vector with mean retention times per row in \code{chromatograms}.
-#'
-#' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
-#'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
-#'
-#' @export
 
-mean_per_row = function(chromatograms, rt_col_name){
-    n_substance <- nrow(chromatograms[[1]]) # all_chromatograms have equal number of rows
-    out <- unlist(lapply(1:n_substance,
-                    function(x) mean_of_samples(chromatograms, 1:length(chromatograms), x, rt_col_name)))
-    out
-
-}
 
 
 

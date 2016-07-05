db66490c630900a040542638f2712de013fab306
mottensmann <Meinolf.Ottensmann@web.de>
2016-07-05 11:43:54 +0200

    Template to create a plot function, summarizing the alignment steps. Not excecutable so far!

---
+++ R/plot.GCalign.R
@@ -0,0 +1,12 @@
+#' plot.GCalign
+#' @description
+#' plot.GCalign creates basic plots visualising the alignment accuracy of chromatograms that
+#' have been processed by \code{\link{align_chromatograms}}
+#'
+#' @author Martin Stoffel (martin.adam.stoffel@@gmail.com) & Meinolf Ottensmann
+#'  (meinolf.ottensmann@@web.de)
+#'
+#'  @export
+plot.GCalign <- function(GcOut){
+
+    }

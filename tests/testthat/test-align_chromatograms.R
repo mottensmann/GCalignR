a00f793ea555bdf160fc0122003979e7b6519307
mottensmann <Meinolf.Ottensmann@web.de>
2016-07-07 16:20:43 +0200

    merge

--- tests/testthat/test-align_chromatograms.R
+++ tests/testthat/test-align_chromatograms.R
@@ -1,36 +1,22 @@
 context("align_chromatograms")
 
-
+library(testthat)
 data("gc_peak_data")
-data <- gc_peak_data[1:2]
+data <- gc_peak_data[1:3]
 
+set.seed(1)
 out <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "RT", write_output = NULL,
         rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = "ind1",
         max_linear_shift = 0.05, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = NULL,
         delete_single_peak = FALSE, n_iter=1)
 
-# test_that("variance is minimized", {
-#     expect_equal()
-# })
+test_that("output is correct", {
+    expect_equal(out$summary$No_Peaks_aligned, 162)
+    expect_equal(round(out$summary$variance_aligned$average, 6), 0.000909)
+})
 
-out <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "RT", write_output = NULL,
-    rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = "ind1",
-    max_linear_shift = 0, max_diff_peak2mean = 0, min_diff_peak2peak = 0, blanks = NULL,
-    delete_single_peak = FALSE, n_iter=1)
 
-# test_that("zeros for all three main parameters steps work", {
-#     expect_equal()
-# })
-#
 
-out <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "RT", write_output = NULL,
-    rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = "ind1",
-    max_linear_shift = 0.05, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = NULL,
-    delete_single_peak = FALSE, n_iter=2)
-
-# test_that("more than one iteration works", {
-#     expect_equal()
-# })
 
 
 

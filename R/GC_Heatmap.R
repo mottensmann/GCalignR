3b11bd9c745c2c3b10e1886d92606d7a670fc0a7
mottensmann <Meinolf.Ottensmann@web.de>
2016-08-18 19:30:21 +0200

    Major update: Fixed some bugs & revised plots. Enhanced flexibility by allow label_size to be set

--- R/GC_Heatmap.R
+++ R/GC_Heatmap.R
@@ -1,4 +1,4 @@
-#' gc_heatmap visualises the goodness of the chromatogram alignment
+#' gc_heatmap visualises the goodness of a chromatogram alignments
 #'
 #' @description Visualises the deviation of single samples from the whole population by comparing
 #' the actual retention time against the mean of all other samples containing the same
@@ -7,23 +7,22 @@
 #' by setting a fixed threshold of allowed deviations from the mean. The optional discrete
 #' heatmap allows to check the deviations quantitatively.
 #'
-#' @param GcOut object of class "GCalign", the output of align_chromatograms.
+#'
+#' @param object object of class "GCalign", the output of a call to \link{align_chromatograms}.
 #'
 #' @param algorithm_step
-#' character string, indicating which step of the algorithm is plotted. Either
-#' \strong{input_rts}, \strong{linear_transformed_rts} or \strong{aligned_rts}
+#' character indicating which step of the algorithm is plotted. Either \strong{input_rts}, \strong{linear_transformed_rts} or \strong{aligned_rts}.
 #'
 #' @param substance_subset
-#' Vector containing indices of substances (i.e. rows) to plot
-#' By default \code{NULL} indicating all substances are plotted
+#' Vector containing indices of substances (ordered in ascending order of retention times) to plot. By default \code{NULL} indicating all substances are plotted.
 #'
 #' @param guide
-#' Character string, selects the type of colourbar as discrete (i.e 'legend')
-#' or gradient (i.e 'colourbar)
+#' Character string, selects the type of colourbar as discrete (i.e \strong{'legend'})
+#' or gradient (i.e \strong{'colourbar})
 #'
 #' @param  samples_subset
 #' Vector indicating which samples are plotted on the heatmap.
-#' Either a numeric vector of indices or a vector of sample names
+#' Either a numeric vector of indices (order in the input) or a vector of sample names
 #'
 #' @param  type
 #' Character specifying whether a binary heatmap or a heatmap of continous
@@ -32,6 +31,9 @@
 #' @param threshold
 #' Decimal indicating the threshold deviation of individual peak retention times
 #' from the mean retention time of the respective peak across all samples.
+#'
+#'@param label_size
+#'Determines the size of labels on y and x axis. By default the label_size is calculated (beta!) to compromise between readibility and messines due to a potentially large number of substances and samples. Note: Label for substances on the x axis are only plotted if a subset of substances was selected, or less than xxx substances are present in the data.
 #'
 #' @return
 #' object of class "ggplot"
@@ -43,67 +45,63 @@
 #'
 #' @examples
 #'
-#'  ## with defaults
+#'  ## Default settings: The final output is plotted
 #'  gc_heatmap(aligned_peak_data, algorithm_step="aligned_rts")
 #'
-#'  ## input data
+#'  ## Plot the input data
 #'  gc_heatmap(aligned_peak_data,algorithm_step="input_rts")
 #'
-#'  ## subset of the first 50 Peaks
+#'  ## Plot a subset of the first 50 scored substances
 #'  gc_heatmap(aligned_peak_data,algorithm_step="aligned_rts",substance_subset = 1:50)
 #'
-#'  ## subset of individuals, stricter threshold
+#'  ## Plot specific samples, apply a stricter threshold
 #'  gc_heatmap(aligned_peak_data,samples_subset = c("M2","P7","M13","P13"),threshold=0.02)
 #'
 #' @export
 #'
 
-gc_heatmap <-function(GcOut,algorithm_step=c('aligned_rts','linear_transformed_rts','input_rts'),substance_subset=NULL,guide=c('legend','colourbar'),
-                      samples_subset=NULL,type=c("binary","continous"),threshold=0.05){
+gc_heatmap <-function(object,algorithm_step=c('aligned_rts','linear_transformed_rts','input_rts'),substance_subset=NULL,guide=c('legend','colourbar'),samples_subset=NULL,type=c("binary","continous"),threshold=0.05,label_size=NULL){
 
+## Grip called parameters
     algorithm_step <- match.arg(algorithm_step)
     type <- match.arg(type)
-    #########################################
-    # A. Select retention times to visualise
-    ########################################
-    rt_df <- GcOut[['heatmap_input']][[algorithm_step]]
+    guide <- match.arg(guide)
 
-    ##########################
-    # B Formatting and sorting
-    ##########################
+## Get the retention time matrix for the selected step
+    rt_df <- object[['heatmap_input']][[algorithm_step]]
     rt_df[,'id'] <- as.character(rt_df[,'id'])
-    rt_df <- rt_df[match(as.character(GcOut[["heatmap_input"]][["input_rts"]][,1]),as.character(rt_df[,1])),]
 
+## Try to estimate a suitable label_size
+if(is.null(label_size)){
+    lab_thresh <- c(20,40,60,80,100,120,140,999) # should be coded saver!
+    lab_size <- c(12,10,8,8,6,5.5,4,4)
+    samples_size <- nrow(rt_df)
+    ## find the matching size
+    temp <- which(lab_thresh > samples_size)
+    label_size <- lab_size[min(temp)-1]
+}
+## Get the initial order, already coded in align_chromatograms
+    # rt_df <- rt_df[match(as.character(object[["heatmap_input"]][["input_rts"]][,1]),as.character(rt_df[,1])),]
 
-    ##################################################
-    # C select a subset of substances, by their position
-    ##################################################
-    if(!is.null(substance_subset)){
+## Select subsets if specified
+    if(!is.null(substance_subset)){ # substances
         rt_df <- rt_df[,c(1,substance_subset+1)] # always keep id !, therefore +1
     }
-    #############################################
-    # D select a subset of samples, by their names
-    #############################################
 
-    if(!is.null(samples_subset)){
+    if(!is.null(samples_subset)){ # samples
         if(is.character(samples_subset)){
             rt_df <- rt_df[rt_df[,1]%in%samples_subset,]
         }else if(is.numeric(samples_subset)){
             rt_df <- rt_df[samples_subset,]
         }
     }
-    ############################################
-    # E Formatting for ggplot2
-    ############################################
+## Create a data frame for plotting with ggolot
+
     heat_matrix <- reshape2::melt(data = rt_df,id.vars ='id')
     names(heat_matrix) <- c('id','substance','rt')
     heat_matrix[,'substance'] <- as.numeric(as.character(heat_matrix[,'substance']))
 
-
-    ###########################################
-    # F Estimate difference from mean of each row
-    ###########################################
-
+## Calculate the deviation of each peak from its substance mean
     heat_matrix[,'diff'] <- (as.numeric(heat_matrix[,'rt']) - heat_matrix[,'substance'])
     heat_matrix['diff'][heat_matrix['rt']==0] <- 0 # zero mean no substance is present
 
@@ -111,23 +109,21 @@ gc_heatmap <-function(GcOut,algorithm_step=c('aligned_rts','linear_transformed_r
     heat_matrix[,'substance'] <- ordered( heat_matrix[,'substance'], levels = as.factor(colnames(rt_df)[2:ncol(rt_df)]))
 
 
-    ###################
-    # G Binary coding
-    ###################
 
-    ldw <- getOption("warn")
-    options(warn = -1)
-
+## If binary heatmap was selected, code violoation at the level of the threshold by 0/1
     if(type=="binary"){
 
         heat_matrix['diff'][abs(heat_matrix['diff'])>threshold] <- 1 # Deviates
         heat_matrix['diff'][abs(heat_matrix['diff'])<threshold] <- 0 # Is okay
         heat_matrix['diff'][heat_matrix['rt']==0] <- NA
     }
+## Code absence by NA
+heat_matrix['diff'][heat_matrix['rt']==0] <- NA
 
-    ###################
-    # Plot the Heatmap
-    ###################
+## Simplify substance names
+heat_matrix['substance'] <- as.factor(round(as.numeric(as.character(heat_matrix[['substance']])),digits = 2))
+
+## Plot the heatmap
 
     if(type=="binary"){
         if(max(heat_matrix['diff'],na.rm = T)==0){ # Zeros indicates no deviations of any substance
@@ -137,41 +133,71 @@ gc_heatmap <-function(GcOut,algorithm_step=c('aligned_rts','linear_transformed_r
             hm <- hm + labs(x=NULL, y=NULL, title=paste("No deviations exceeding a threshold of",as.character(threshold)))
             hm <- hm + guides(fill=FALSE)
 
-        }else if(min(heat_matrix['diff'],na.rm = T)==1){ # Really bad alignment
-            hm <- ggplot(heat_matrix, aes_string(x='substance', y='id',fill=diff),colour="red")
-            hm <- hm + geom_tile(color="transparent", size=0.001)
-            hm <- hm + scale_fill_gradientn(colours = 'red',na.value = "white")
-            hm <- hm + labs(x=NULL, y=NULL, title=paste("Alignment failed, all substances show deviation from threshold of",as.character(threshold)))
-            hm <- hm + guides(fill=FALSE)
 
-        }else{ # Should be the general outcome, some samples still deviate
+## Case is not possible, therefore exluded
+        # }else if(min(heat_matrix['diff'],na.rm = T)==1){ # Really bad alignment
+        #     hm <- ggplot(heat_matrix, aes_string(x='substance', y='id',fill=diff),colour="red")
+        #     hm <- hm + geom_tile(color="transparent", size=0.001)
+        #     hm <- hm + scale_fill_gradientn(colours = 'red',na.value = "white")
+        #     hm <- hm + labs(x=NULL, y=NULL, title=paste("Alignment failed, all substances show deviation from threshold of",as.character(threshold)))
+        #     hm <- hm + guides(fill=FALSE)
+
+        }else{ # At leat some samples deviate at certain retention times
             hm <- ggplot(heat_matrix, aes_string(x='substance', y='id',fill='diff'))
             hm <- hm + geom_tile(color="transparent", size=0.001)
-            hm <- hm + scale_fill_continuous(low = "yellow",high = "black",breaks=c(0,1),na.value = "white",
+            hm <- hm + scale_fill_continuous(low = "#a6cee3",high = "#b2182b",breaks=c(0,1),na.value = "white",
                                              guide = 'legend',name=paste('Deviation\n','>',as.character(threshold)),labels=c('NO','YES'))
             hm <- hm + labs(x=NULL, y=NULL, title=paste("Deviations of retention times at a threshold of",as.character(threshold)))
-
         }
     }else{
-        myPalette <-  colorRampPalette(rev(RColorBrewer::brewer.pal(11, "BrBG"))) # take a colour
+        myPalette <-  colorRampPalette(rev(RColorBrewer::brewer.pal(11, "Spectral"))) # Spectral colours
+        colourset <- myPalette(10) # Take 10 colours form the spectral scheme
+        colourset <- colourset[c(1:4,7:10)] # remove middle ones for higher contrast to background
         hm <- ggplot(heat_matrix, aes_string(x='substance', y='id', fill='diff'))
         hm <- hm + geom_tile(color="white", size=0.01)
-        hm <- hm + scale_fill_gradientn(colours = myPalette(10),guide = guide,name='Deviation')
-        hm <- hm + labs(x=NULL, y=NULL, title="Deviation of retention times")
+        hm <- hm + scale_fill_gradientn(colours = myPalette(10),guide = guide,name='Deviation',na.value = "white")
+        hm <- hm + labs(x=NULL, y=NULL, title="Deviation of individual peak retentention times from the substance mean")
 
     }
-    hm <- hm + theme(plot.title=element_text(hjust=0,size = 12,face = 'bold'))
+    hm <- hm + theme(plot.title=element_text(hjust=0.5,vjust=1,size = 16,face = 'bold'))
     hm <- hm + theme(axis.title.x=element_blank(),
                      axis.text.x=element_blank(),
-                     axis.ticks=element_blank(),
-                     axis.text.y=element_text(size = 5))
-    hm <- hm + coord_equal(ncol(rt_df)/nrow(rt_df))
+                     axis.ticks.y = element_line(size = 0.3, colour = "grey40"),
+                     axis.ticks.x = element_line(size = 0.3, colour = "grey40"),
+                     axis.text.y=element_text(size = label_size,hjust = 0.5))
+    #hm <- hm + coord_equal(ncol(rt_df)/nrow(rt_df))
 
-    # return(suppressWarnings(hm))
-    # options(warn = oldw)
-    return(hm)
+hm <- hm + theme(plot.background = element_rect(fill = "grey95"))
+
+y <- 1:nrow(rt_df)+.5
+x <- rep(0,nrow(rt_df))
+yend <- 1:nrow(rt_df)+.5
+xend <- rep(ncol(rt_df),nrow(rt_df))
+my.lines<-data.frame(y=y,x=x,xend=xend, yend=yend)
+
+hm <- hm + geom_segment(data=my.lines, aes(x,y,xend=xend, yend=yend),color="grey", size=0.35,show.legend = FALSE, inherit.aes=F)
+
+y <- rep(0,ncol(rt_df))
+x <- 1:ncol(rt_df)+.5
+xend <- 1:ncol(rt_df)+.5
+yend <- rep(nrow(rt_df)+.5,ncol(rt_df))
+my.lines <- data.frame(y=y,x=x, xend=xend,yend=yend)
+
+hm <- hm + geom_segment(data=my.lines, aes(x,y,xend=xend, yend=yend),color="grey", size=0.35,show.legend = FALSE, inherit.aes=F)
+
+
+
+## If subsets are selected, allow to plot the substance labels for better idetenfication
+if(!is.null(substance_subset)){
+    hm <- hm + theme(axis.title.x=element_blank(),
+                     axis.text.x=element_text(size = label_size,hjust = 0.5,angle = 90),
+                     axis.ticks.y = element_line(size = 0.3, colour = "grey60"),
+                     axis.ticks.x = element_line(size = 0.3, colour = "grey60"),
+                     axis.text.y=element_text(size = label_size,hjust = 0.5))
 }
 
+return(hm)
 
+}
 
 

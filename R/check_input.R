83e553dddc6004c0e1899e3f2ece8ffad5c1ee92
mottensmann <Meinolf.Ottensmann@web.de>
2016-11-30 16:58:23 +0100

    Exchanged most stops with warnings. Therefore, the code only breaks when the target is not available. FALSE is returned if any essential prerequisite is not matched. A statement asks user to change them accordingly

--- R/check_input.R
+++ R/check_input.R
@@ -33,6 +33,8 @@
 #'
 check_input <- function(data,list_peaks = FALSE, sep = "\t", ...) {
 
+    # Preallocate a flag for passing the test. Every severe issues sets pass to FALSE
+    pass = TRUE
     ## Get the name of "data" and optional parameters
     mcall = as.list(match.call())[-1L]
     opt <- list(...)
@@ -66,8 +68,14 @@ check_input <- function(data,list_peaks = FALSE, sep = "\t", ...) {
 
         ### Check input for completeness ###
 
-        if (!((ncol(gc_data) / length(col_names)) %% 1) == 0) stop("Number of data columns is not a multiple of the column names provided")
-        if (!((ncol(gc_data) / length(col_names))  == length(ind_names))) stop("Number of sample names provided does not fit to the number of columns in the data")
+        if (!((ncol(gc_data) / length(col_names)) %% 1) == 0) {
+            pass <- FALSE
+        warning("Number of data columns is not a multiple of the column names provided")
+        }
+        if (!((ncol(gc_data) / length(col_names))  == length(ind_names))) {
+            pass <- FALSE
+            warning("Number of sample names provided does not fit to the number of columns in the data")
+        }
         if (any(duplicated(ind_names))) warning("Avoid duplicates in sample names")
         ## convert to list
         gc_peak_list <- conv_gc_mat_to_list(gc_data, ind_names, var_names = col_names)
@@ -75,11 +83,20 @@ check_input <- function(data,list_peaks = FALSE, sep = "\t", ...) {
 ## If data is a list of data.frames
     } else if (is.list(data)) {
         ## check that every element in the list is a data.frame
-        if (!(all(unlist(lapply(data, is.data.frame))))) stop("Every Sample has to be a data.frame")
+        if (!(all(unlist(lapply(data, is.data.frame))))) {
+            pass <- FALSE
+            warning("Every Sample has to be a data.frame")
+        }
         ## check all data.frames are named
-        if ((is.null(names(data)))) stop("Every data.frame needs to be named with the sample id")
+        if ((is.null(names(data)))) {
+            pass <- FALSE
+        warning("Every data.frame needs to be named with the sample id")
+        }
         col_names <- unlist(lapply(data, function(x) out <- names(x)))
-        if (any(table(col_names) != length(data))) stop("Every sample needs to have the same number of columns")
+        if (any(table(col_names) != length(data))) {
+            pass <- FALSE
+            warning("Every sample needs to have the same number of columns")
+        }
         col_names <- names(data[[1]])
         ind_names <- names(data)
         if (any(duplicated(ind_names))) warning("Avoid duplicates in sample names")
@@ -88,20 +105,30 @@ check_input <- function(data,list_peaks = FALSE, sep = "\t", ...) {
 
 ## Do some internal checks if write_output is defined in align_chromatograms
         if (any(names(opt) == "write_output")) {
-        if (any(!(opt[["write_output"]] %in% col_names))) stop("Names in write_output have to be included as a variable in the data!")
-    }
+        if (any(!(opt[["write_output"]] %in% col_names))) {
+            pass <- FALSE
+            warning("Names in write_output have to be included as a variable in the data!")
+        }
+            }
     if (any(stringr::str_detect(string = ind_names, pattern = " "))) warning("Avoid whitespaces in Sample Names!")
     if (any(stringr::str_detect(string = ind_names, pattern = "[^a-zA-Z\\d\\_]"))) warning("Sample Names should only contain Letters, Numbers and '_' ")
     if (any(stringr::str_detect(string = col_names, pattern = " "))) warning("Avoid whitespaces in Variable Names!")
     if (any(stringr::str_detect(string = col_names, pattern = "[^a-zA-Z\\d\\_]"))) warning("Variable Names should only contain Letters, Numbers and '_' ")
 
     if (any(names(opt) == "blank")) {
-        if (any(!(opt[["blank"]] %in% ind_names))) stop("blanks have to refer to samples in the data!")
+        if (any(!(opt[["blank"]] %in% ind_names))) {
+            pass <- FALSE
+        warning("blanks have to refer to samples in the data!")
+        }
     }
     if (any(names(opt) == "reference")) {
-        if (!is.null(opt[["reference"]]) & any(!(opt[["reference"]] %in% ind_names))) stop("reference has to be included as a sample in the data!")
-    }
+        if (!is.null(opt[["reference"]]) & any(!(opt[["reference"]] %in% ind_names))) {
+    pass <- FALSE
+    warning("reference has to be included as a sample in the data!")
+}
+}
     format_error <- function(x){
+        pass <- TRUE
         check_var_count <- function(x) {
             mat <- as.matrix(x)
             L <- length(unique(colSums(!is.na(x))) == 1)
@@ -111,13 +138,20 @@ check_input <- function(data,list_peaks = FALSE, sep = "\t", ...) {
         if (length(which(y != 1)) > 0) {
             out <- names(y[which(y != 1)])
             warning(paste(out,collapse = "; ") ," violate(s) the requirements.",call. = FALSE)
-            stop("Every sample needs to have the same number of values for each variable!",call. = FALSE)
+            warning("Every sample needs to have the same number of values for each variable!",call. = FALSE)
+            pass <- FALSE
         }
+        return(pass)
     }
 
     ## Checks that every sample has the same number of values per column
-    format_error(gc_peak_list)
-    cat("All checks passed!\nReady for processing with align_chromatograms")
+    format_pass <- format_error(gc_peak_list)
+    if (pass == TRUE & format_pass == TRUE) {
+        cat("All checks passed!\nReady for processing with align_chromatograms")
+    } else {
+        cat("Not all checks have been passed. Read warning messages and change data accordingly")
+    }
+
 
     if (list_peaks == TRUE) {
         counter <- function(gc_peak_list){
@@ -158,9 +192,13 @@ check_input <- function(data,list_peaks = FALSE, sep = "\t", ...) {
     if (list_peaks == TRUE) {
         ## Form a data frame with peak numbers
         output <- data.frame(sample = names(peaks), peaks = peaks,row.names = 1:length(peaks))
-    } else{
+    } else {
         ## Give a True for fast checks without plotting
-        output <- "TRUE"
+        if (pass == TRUE & format_pass == TRUE) {
+            output <- "TRUE"
+        } else {
+            output <- "FALSE"
+        }
     }
     ## return
     return.values <- output

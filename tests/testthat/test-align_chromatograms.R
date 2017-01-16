edf96372611c878f4f33dd440e8031655e9304e6
mottensmann <Meinolf.Ottensmann@web.de>
2017-01-16 14:05:16 +0100

    updates after chaning function

--- tests/testthat/test-align_chromatograms.R
+++ tests/testthat/test-align_chromatograms.R
@@ -5,10 +5,10 @@ data("peak_data")
 data <- peak_data[1:3]
 
 set.seed(1)
-out <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "time", write_output = NULL, rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = "M2",max_linear_shift = 0.05, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = "C2",delete_single_peak = FALSE, iterations=1)
+out <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "time", write_output = NULL, rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = "M2",max_linear_shift = 0.05, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = "C2",delete_single_peak = FALSE)
 
 set.seed(1)
-out2 <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "time", write_output = NULL, rt_cutoff_low = 15, rt_cutoff_high = 30, reference = NULL,max_linear_shift = 0.05, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = NULL,delete_single_peak = TRUE, iterations = 2)
+out2 <- align_chromatograms(data, sep = "\t", conc_col_name= "area", rt_col_name = "time", write_output = NULL, rt_cutoff_low = 15, rt_cutoff_high = 30, reference = NULL,max_linear_shift = 0.05, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = NULL,delete_single_peak = TRUE)
 
 test_that("output is correct", {
     expect_equal(out[["Logfile"]][["Aligned"]][["Retained"]], 114) # Number of Peaks

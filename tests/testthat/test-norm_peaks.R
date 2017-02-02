74704f9f2080a4545e3c978d1fd54595bcb1efe2
mottensmann <Meinolf.Ottensmann@web.de>
2017-02-02 21:09:01 +0100

    changed test after new defaults in norm_peaks

--- tests/testthat/test-norm_peaks.R
+++ tests/testthat/test-norm_peaks.R
@@ -3,11 +3,11 @@ context("norm_peaks")
 library(testthat)
 data("aligned_peak_data")
 
-x <- norm_peaks(aligned_peak_data, conc_col_name = "area")
+x <- norm_peaks(aligned_peak_data, conc_col_name = "area",out = "list")
 y <- as.vector(unlist(lapply(x,sum)))
 y <- any(round(y,digits = 10) != 100)
 
-x2 <- norm_peaks(aligned_peak_data, conc_col_name = "area",percent = FALSE)
+x2 <- norm_peaks(aligned_peak_data, conc_col_name = "area",percent = FALSE, out = "list")
 y2 <- as.vector(unlist(lapply(x2, sum)))
 y2 <- any(round(y2,digits = 10) != 1)
 

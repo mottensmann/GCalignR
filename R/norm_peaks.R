2bb7679dfec9aed8a6bb9aa2b844f46e10d1e297
mottensmann <Meinolf.Ottensmann@web.de>
2017-02-05 15:15:26 +0100

    simplified function, added example

--- R/norm_peaks.R
+++ R/norm_peaks.R
@@ -4,42 +4,53 @@
 #' \code{norm_peaks} calculates the relative abundance of a peak by normalising
 #' with regard to the cumulative abundance of all peaks that are present within an
 #' individual chromatogram. The desired measure of peak abundance needs to be a column
-#' within the original gas-chromatography data \code{datafile} submitted to
-#' \link{align_chromatograms}.
+#' within the original gas-chromatography dataset aligned by \link{align_chromatograms}.
 #'
 #' @param data
-#' object of class GCaling created with \link{align_chromatograms}. Contains a list
-#' of data.frames including the retention time and other variables, of which one needs
-#' to be named as specified by \code{conc_col_name}.
+#' Object of class GCalign created with \link{align_chromatograms}. Contains a list
+#' of data frames including the retention time and other variables, of which one needs
+#' to be specified as \code{conc_col_name}.
 #'
 #' @param conc_col_name
-#' character string denoting a column in data.frames of \code{data}
+#' Character string denoting a column in data frames of \code{data}
 #' containing a variable describing the abundance of peaks (e.g. peak area or peak height).
-#'
-#' @param percent
-#' logical, if \code{percent = TRUE} normalised conentraion scale up to 100, otherwise to 1.
-#'
-#' @param which
-#' character string naming the data source to normalise. Either the aligned data or the raw data prior to alignment. The latter can be used for demonstration purposes.
 #'
 #' @param out
 #' character string defining the format of the returned data. Either "List" or "data.frame".
 #'
+#' @inheritParams align_chromatograms
+#'
 #' @return
-#' depending on \code{out} either a list of data frame or a single data frame were rows represent samples and columns relative peak abundancies.
+#' Depending on \code{out} either a list of data frame or a single data frame were rows represent samples and columns relative peak abundancies. Abundancies are given in percent.
 #'
 #'  @author Martin Stoffel (martin.adam.stoffel@@gmail.com) &
 #'         Meinolf Ottensmann (meinolf.ottensmann@@web.de)
+#' @examples
+#' ## aligned gc-dataset
+#' data("aligned_peak_data")
+#' ## returns normalised peak area
+#' norm_peaks(data = aligned_peak_data, conc_col_name = "area", rt_col_name = "time")
 #'
-#' @keywords internal
 #' @export
 #'
-norm_peaks <- function(data,conc_col_name = NULL, rt_col_name = NULL, out = c("data.frame","list"), percent = TRUE, which = c("aligned","raw")) {
-out <- match.arg(out)
-which <- match.arg(which)
+norm_peaks <- function(data, conc_col_name = NULL, rt_col_name = NULL, out = c("data.frame","list")) {
 
-if(which == "raw") which <- "input_matrix"
-# some checks
+out <- match.arg(out)
+which <- "aligned" # which <- match.arg(which)
+
+## unused code chunks
+## ----------------------------------------------
+# if (which == "raw") which <- "input_matrix"
+## additional function parameters
+#  ..., percent = TRUE, which = c("aligned","raw")
+# param percent
+# By default percent values are returned (i.e. relative abundancies scale up to 100.) If
+#
+# param which
+# character string naming the data source to normalise. Either the aligned data or the raw data prior to alignment. The latter can be used for demonstration purposes.
+# -----------------------------------------------
+
+## some checks
 if (class(data) != "GCalign") {warning("Input is not a output of align_chromatograms, assure the format is correct")}
 if (is.null(conc_col_name)) {stop("List containing peak concentration is not specified. Define conc_col_name")}
 
@@ -49,13 +60,15 @@ conc_list <- data[[which]][[conc_col_name]]
 rel_abund <- function(conc_df){
     total_con <- sum(conc_df)
     conc_df <- (conc_df/total_con)
-    if (percent == TRUE) conc_df <- conc_df * 100
+    ## if (percent == TRUE)
+    conc_df <- conc_df * 100
     return(conc_df)
 }
 
 # Lapply over all data.frames
 rel_con_list <- lapply(conc_list, rel_abund)
 if (out == "data.frame") {
+    if (is.null(rt_col_name)) stop("Specify rt_col_name")
         x <- rel_con_list[-1]
         x <- as.data.frame(t(do.call(cbind,x)))
         colnames(x) <- data[[which]][[rt_col_name]]["mean_RT"][[1]]
--- tests/testthat/test-norm_peaks.R
+++ tests/testthat/test-norm_peaks.R
@@ -5,12 +5,13 @@ data("aligned_peak_data")
 
 x <- norm_peaks(aligned_peak_data, conc_col_name = "area",out = "list")
 y <- as.vector(unlist(lapply(x,sum)))
+## supposed to be an artefact, values deviate from 100 by approx. e-15
 y <- any(round(y,digits = 10) != 100)
 
-x2 <- norm_peaks(aligned_peak_data, conc_col_name = "area",percent = FALSE, out = "list")
-y2 <- as.vector(unlist(lapply(x2, sum)))
-y2 <- any(round(y2,digits = 10) != 1)
-
+x2 <- norm_peaks(aligned_peak_data, conc_col_name = "area", rt_col_name = "time", out = "data.frame")
+## supposed to be an artefact, values deviate from 100 by approx. e-15
+y2 <- as.vector(rowSums(x2))
+y2 <- any(round(y2) != 100)
 
 test_that("output is correct", {
     expect_equal(y, FALSE)

8b52efd0603f5fa536877ee2914e6a05c5fa26ca
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2017-07-04 18:59:43 +0200

    adjusted test

--- tests/testthat/test-draw_chromatograms.R
+++ tests/testthat/test-draw_chromatograms.R
@@ -3,11 +3,11 @@ context("draw_chromatogram")
 library(testthat)
 data("aligned_peak_data")
 
-x <- draw_chromatogram(data = aligned_peak_data, rt_col_name = "time", samples = "M2", add_num = FALSE, plot = FALSE)
+x <- draw_chromatogram(data = aligned_peak_data, rt_col_name = "time", samples = "M2", show_num = FALSE, plot = FALSE)
 
 test_that("output is correct", {
     expect_equal(length(x), 2) # list of two elements
-    expect_equal(dim(x[["df"]]), c(10000, 3))
+    expect_equal(dim(x[["df"]]), c(8622, 3))
 })
 
 

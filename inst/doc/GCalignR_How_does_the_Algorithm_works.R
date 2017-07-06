4c1cb1c71e928ac157c4dd539def07b497b60381
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2017-07-06 19:24:01 +0200

    updates

--- inst/doc/GCalignR_How_does_the_Algorithm_works.R
+++ inst/doc/GCalignR_How_does_the_Algorithm_works.R
@@ -75,46 +75,113 @@ for (i in levels(peaks[["sample"]])) {
     dat_mat <- cbind(dat_mat, temp)
 } 
 ## output the formatted matrix
-write.table(dat_mat, row.names = F, col.names = F, sep = "\t")
+write.table(round(dat_mat,2), row.names = F, col.names = F, sep = "\t")
 
-## ---- echo=FALSE, fig.cap="Figure 4. Corrected linear drift between reference and sample"----
-# graphical representation of the procedure that is applied during the correction of linear drift.
-set.seed(1533)
-# create two samples
-df <- rbind(simple_chroma(peaks = c(10,15,22), N = 1, Names = "Reference"), simple_chroma(peaks = c(12,16.89,24), N = 1, Names = "Sample"))
-peaks <- find_peaks(df)
+## ---- echo = FALSE, fig.cap="Figure 4. Corrected linear drift between Reference and Focal sample at a shift of -1"----
+# create two chromatograms
+df1 <- data.frame(simple_chroma(peaks = c(5,10,16,20,24),
+                                N = 1,
+                                Names = "Reference",
+                                sd = c(0.30, 0.35, 0.24, 0.25, 0.23)),
+                                ym1 = NA, ym2 = NA, yp1 = NA, yp2 = NA)
+
+x <- c(6,11.2,20.95)
+df2 <- data.frame(simple_chroma(peaks = x,
+                                N = 1,
+                                Names = "Focal sample",
+                                sd = c(0.22, 0.28, 0.27)),
+                  ym1 = data.frame(simple_chroma(peaks = x - 1,
+                                                 N = 1,
+                                                 Names = "Focal sample",
+                                                 sd = c(0.22, 0.28, 0.27)),
+                                   type = "Focal sample")[["y"]],
+                  ym2 = data.frame(simple_chroma(peaks = x - 2,
+                                                 N = 1,
+                                                 Names = "Focal sample",
+                                                 sd = c(0.22, 0.28, 0.27)),
+                                   type = "Focal sample")[["y"]],
+                  yp1 = data.frame(simple_chroma(peaks = x + 1,
+                                                 N = 1,
+                                                 Names = "Focal sample",
+                                                 sd = c(0.22, 0.28, 0.27)),
+                                   type = "Focal sample")[["y"]],
+                  yp2 = data.frame(simple_chroma(peaks = x + 2,
+                                                 N = 1,
+                                                 Names = "Focal sample",
+                                                 sd = c(0.22, 0.28, 0.27)),
+                                   type = "Focal sample")[["y"]])
+df <- rbind(df1,df2)
+
+# reduce range of x
+df <- subset(df, x > 3.5 & x < 27)
+# find peaks
+peaks <- find_peaks(df[,1:3])
 peaks2 <- peaks
-# Adjust retention times to shift the sample chromatogram
-peaks2[["x"]] <- peaks2[["x"]] - 2
-peaks2[["x"]][peaks2[["x"]] < 0] <- 0
+peaks2[["x"]][6:8] <- peaks2[["x"]][c(1:2,4)]
 
-# create a data frame for plotting
-df2 <- data.frame(x = rep(seq(0,30,length = 10000),2), y = c(GCalignR:::peaks2chroma(data = peaks, sample = "Reference"), GCalignR:::peaks2chroma(data = peaks, sample = "Sample")), sample = rep(c("Reference", "Sample A"), each = 10000), y2 = c(rep(0, 10000),GCalignR:::peaks2chroma(data = peaks2, sample = "Sample")))
+tx <- peaks[["x"]][peaks[["sample"]] == "Focal sample"]
+tex <- data.frame(x = c(tx - 2, tx - 1, tx, tx + 1, tx + 2), y = rep(peaks[["y"]][peaks[["sample"]] == "Focal sample"],5), z = rep(-2:2, each = 3), sample = "Focal sample")
 
-#subset on x
-df2 <- subset(df2, df2$x > (min(peaks[["x"]]) - 1) & df$x < (max(peaks[["x"]]) + 1))
 
-# plot
-plot <- ggplot(df2, aes(x = x, y = y, col = sample, fill = sample)) +
-    geom_line(size = 1.2) +
-    theme_classic() + xlab("Retention time ") + ylab("Intensity") +
-    scale_x_continuous(expand = c(0,0)) +
-    theme(axis.text = element_blank(), axis.ticks = element_blank(), legend.position = "bottom") +
-    geom_line(aes(x = x,y = y2), linetype = "dotted", size = 1) +
-    scale_color_manual(values = c("black","black"), guide = FALSE) +
-    geom_area() +
-    scale_fill_manual(values = c("darkorange","blue"), name = "") +
-    geom_area(aes(x = x,y = y2), alpha = 0.6) +
-    geom_segment(aes(x = peaks[peaks[["sample"]] == "Sample",][2,1], xend = peaks2[peaks2[["sample"]] == "Sample",][2,1], y = find_peaks(df2)[5,2] + 0.05, yend = find_peaks(df2)[5,2] + 0.05),size = 1.2, colour = "black", arrow = arrow(length = unit(x = 0.14, units = "cm"))) +
-    annotate("text", x = find_peaks(df2)[5,1] - 1, y = find_peaks(df2)[5,2] + 0.25, label = "Linear shift", angle = 0) + xlab(label = "") + ylab(label = "") 
-print(plot)
+chroma <- ggplot(data = df, aes(x,y, fill = sample)) + geom_line(size = 1.2, colour = "black") +
+    theme_classic(base_size = 12) + xlab("Retention time ") + ylab("") +
+    scale_x_continuous(breaks = seq(4,26,1),expand = c(0,0)) +
+    theme(axis.text.y = element_blank(), axis.ticks = element_blank(),
+          strip.background = element_rect(colour = "black", fill = "#CCCCFF")) +
+    facet_wrap(~sample, ncol = 1) +
+    scale_fill_manual(values = c("#1B9E77","#F16913")) +
+    theme(legend.position = "none", axis.text.x = element_blank()) +
+    geom_area(alpha = 0.5) +
+    geom_line(aes(x = x, y = ym1), colour = "#FD8D3C", size = 1.2, linetype = "solid") +
+    geom_line(aes(x = x, y = ym2), colour = "#FDAE6B", size = 0.8, linetype = "solid") +
+    geom_line(aes(x = x, y = yp1), colour = "#D94801", size = 0.8, linetype = "solid") +
+    geom_line(aes(x = x, y = yp2), colour = "#A63603", size = 0.8, linetype = "solid") +
+    geom_text(data = tex, aes(x = x, y = y + 0.1), label = as.character(tex[["z"]])) +
+    geom_rect(data = peaks2, aes(xmin = x - 0.1, xmax = x + 0.1, ymin = 0, ymax = y), linetype = "solid", colour = "black", fill = "grey50", size = 0.8, alpha = 0.2)
+print(chroma)
+
+## ---- echo = FALSE, fig.cap="Figure 4. Corrected linear drift between reference and sample", eval=FALSE----
+#  # REPLACED BY EXAMPLE ABOVE
+#  # graphical representation of the procedure that is applied during the correction of linear drift.
+#  set.seed(1533)
+#  # create two samples
+#  df <- rbind(simple_chroma(peaks = c(10,15,22), N = 1, Names = "Reference"), simple_chroma(peaks = c(12,16.89,24), N = 1, Names = "Sample"))
+#  peaks <- find_peaks(df)
+#  peaks2 <- peaks
+#  # Adjust retention times to shift the sample chromatogram
+#  peaks2[["x"]] <- peaks2[["x"]] - 2
+#  peaks2[["x"]][peaks2[["x"]] < 0] <- 0
+#  
+#  # create a data frame for plotting
+#  df2 <- data.frame(x = rep(seq(0,30,length = 10000),2), y = c(GCalignR:::peaks2chroma(data = peaks, sample = "Reference"), GCalignR:::peaks2chroma(data = peaks, sample = "Sample")), sample = rep(c("Reference", "Sample A"), each = 10000), y2 = c(rep(0, 10000),GCalignR:::peaks2chroma(data = peaks2, sample = "Sample")))
+#  
+#  #subset on x
+#  df2 <- subset(df2, df2$x > (min(peaks[["x"]]) - 1) & df$x < (max(peaks[["x"]]) + 1))
+#  
+#  # plot
+#  plot <- ggplot(df2, aes(x = x, y = y, col = sample, fill = sample)) +
+#      geom_line(size = 1.2) +
+#      theme_classic() + xlab("Retention time ") + ylab("Intensity") +
+#      scale_x_continuous(expand = c(0,0)) +
+#      theme(axis.text = element_blank(), axis.ticks = element_blank(), legend.position = "bottom") +
+#      geom_line(aes(x = x,y = y2), linetype = "dotted", size = 1) +
+#      scale_color_manual(values = c("black","black"), guide = FALSE) +
+#      geom_area() +
+#      scale_fill_manual(values = c("darkorange","blue"), name = "") +
+#      geom_area(aes(x = x,y = y2), alpha = 0.6) +
+#      geom_segment(aes(x = peaks[peaks[["sample"]] == "Sample",][2,1], xend = peaks2[peaks2[["sample"]] == "Sample",][2,1], y = find_peaks(df2)[5,2] + 0.05, yend = find_peaks(df2)[5,2] + 0.05),size = 1.2, colour = "black", arrow = arrow(length = unit(x = 0.14, units = "cm"))) +
+#      annotate("text", x = find_peaks(df2)[5,1] - 1, y = find_peaks(df2)[5,2] + 0.25, label = "Linear shift", angle = 0) + xlab(label = "") + ylab(label = "")
+#  print(plot)
+
+## ---- echo=FALSE, fig.cap="Figure 5. Alignment of individual peaks based on retention time matrices. Colours represent substances, black rectangles highlight causes of manipulations.",out.width = "750px"----
+knitr::include_graphics("align_peaks.png",dpi = 300)
 
 ## ---- fig.cap="Figure 5. Chromatographic representation of the dataset prior to alignment"----
 ## path to the data
 path <- system.file("extdata", "simulated_peak_data.txt", package = "GCalignR")
 ## draw chromatograms
 x <- draw_chromatogram(data = path, rt_col_name = "rt", show_rt = T, show_num = F, plot = F)
-x[["ggplot"]] + geom_line(size = 1.2) + theme(axis.ticks.x = element_blank())
+x[["ggplot"]] + geom_line(size = 1.2) + theme(axis.ticks.x = element_blank()) + ggplot2::scale_color_brewer(palette = "Dark2")
 
 ## ---- eval=T, results="hide"---------------------------------------------
 aligned <- align_chromatograms(data = path,
@@ -128,13 +195,15 @@ aligned <- align_chromatograms(data = path,
 print(aligned[["Logfile"]][["LinearShift"]])
 
 ## ---- results="hide"-----------------------------------------------------
-x <- draw_chromatogram(data = aligned, rt_col_name = "rt", step = "lin_aligned", show_rt = F, show_num = F)
+x <- draw_chromatogram(data = aligned, rt_col_name = "rt", step = "lin_aligned", show_rt = F, show_num = F, plot = F)
+x[["ggplot"]] + ggplot2::scale_color_brewer(palette = "Dark2")
 
 ## ---- results="hide"-----------------------------------------------------
-x <- draw_chromatogram(data = aligned, rt_col_name = "rt", step = "fully_aligned", show_num = T)
+x <- draw_chromatogram(data = aligned, rt_col_name = "rt", step = "fully_aligned", show_num = T, plot = F)
+x[["ggplot"]] + ggplot2::scale_color_brewer(palette = "Dark2")
 
 ## ------------------------------------------------------------------------
 ## for using ggplot2::facet_wrap we need to get rid of the annotations
 x <- draw_chromatogram(data = aligned, rt_col_name = "rt", step = "fully_aligned", show_num = F, plot = F)
-x[["ggplot"]] + ggplot2::facet_wrap(~sample, ncol = 1)
+x[["ggplot"]] + ggplot2::facet_wrap(~sample, ncol = 1) + ggplot2::scale_color_brewer(palette = "Dark2")
 

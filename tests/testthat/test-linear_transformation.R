61175ac4fb82d778eb2c986edf3085421caf4706
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2017-07-13 17:29:30 +0200

    update

--- tests/testthat/test-linear_transformation.R
+++ tests/testthat/test-linear_transformation.R
@@ -11,9 +11,19 @@ out <- align_chromatograms(data, sep = "\t", rt_col_name = "time", write_output 
 set.seed(1)
 out2 <- align_chromatograms(data, sep = "\t", rt_col_name = "time", write_output = NULL, rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = "M2",max_linear_shift = 0, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = "C2",delete_single_peak = FALSE)
 
+set.seed(1)
+out4 <- align_chromatograms(data, sep = "\t", rt_col_name = "time", write_output = NULL, rt_cutoff_low = NULL, rt_cutoff_high = NULL, reference = NULL,max_linear_shift = 0, max_diff_peak2mean = 0.02, min_diff_peak2peak = 0.03, blanks = "C2",delete_single_peak = FALSE)
+
+
+path <- system.file("extdata", "simulated_peak_data.txt", package = "GCalignR")
+list <- read_peak_list(data = path, rt_col_name = "rt")
+out3 <- GCalignR:::linear_transformation(gc_peak_list = list, reference = "A2", max_linear_shift = 2, error = 0.05, rt_col_name = "rt", Logbook = list(), step_size = 0.1)
+
+
 test_that("output is correct", {
     expect_equal(sum(out[["heatmap_input"]][["linear_transformed_rts"]][,2:10]), 214.86) #214.95
     expect_equal(sum(out[["heatmap_input"]][["input_rts"]][,2:10]), 214.41) #291
     expect_equal(sum(out2[["heatmap_input"]][["linear_transformed_rts"]][,2:10]), 214.41)
     expect_equal(sum(out2[["heatmap_input"]][["input_rts"]][,2:10]), 214.41) #291
+    expect_equal(sum(out3[["Logbook"]][["LinearShift"]][["shift"]]), -1.6)
 })
